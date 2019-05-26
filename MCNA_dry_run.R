8ab8151b7c0fabe0050827b0235b74ff36ce895b
boukepieter <boukepieter@gmail.com>
2019-05-26 18:33:38 +0300

    recoding Emerg.Livelihoods

--- MCNA_dry_run.R
+++ MCNA_dry_run.R
@@ -7,10 +7,6 @@ library(xlsformfill) # generate fake data for kobo
 library(hypegrammaR) # simple stats 4 complex samples
 library(composr) # horziontal operations
 
-devtools::install_github(
-  "mabafaba/xlsformfill"
-)
-
 source("functions/to_alphanumeric_lowercase.R")
 questions <- read.csv("input/kobo_questions.csv", stringsAsFactors=F, check.names=F)
 
@@ -26,8 +22,20 @@ choices$name <- tolower(choices$name)
 choices$filter <- tolower(choices$filter)
 
 # debugonce(xlsformfill:::remove_skipped_values)
-response <- xlsform_fill(questions,choices,10)
+response <- xlsform_fill(questions,choices,100)
 warnings()
 response[,1:10]
 
-response %>% new_recoding(source=how_much_debt, target=hh_with_debt_value)
+r <- response %>% new_recoding(source=how_much_debt, target=hh_with_debt_value) %>% 
+  recode_to(TRUE,where.num.larger.equal = 505000,otherwise.to=FALSE) %>% 
+  end_recoding()
+
+r <- r %>%  new_recoding(target=hh_unemployed) %>% 
+  recode_to(FALSE,where=!(is.na(response$work) | is.na(response$actively_seek_work))) %>% 
+  recode_to(TRUE,where=(work == "no") & (actively_seek_work == "yes")) %>% 
+  end_recoding()
+
+r %>% new_recoding(source=reasons_for_debt, target=hh_unable_basic_needs) %>% 
+  recode_to(TRUE, where.selected.any = c("health","food","education","basic_hh_expenditure"), otherwise.to=FALSE)
+
+
