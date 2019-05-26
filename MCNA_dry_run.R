050c8e9e9a88cef9274fbd605e1dac24aa55d1eb
Martin <martin.barner@impact-initiatives.org>
2019-05-26 14:15:51 +0300

    dependencies

---
+++ MCNA_dry_run.R
@@ -0,0 +1,10 @@
+
+library(dplyr)
+library(koboquest) # manage kobo questionnairs
+library(kobostandards) # check inputs for inconsistencies
+library(xlsformfill) # generate fake data for kobo
+library(hypegrammaR) # simple stats 4 complex samples
+library(composr) # horziontal operations
+
+
+
