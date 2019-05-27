5d8e95758b67b9a8fff6fde4cbc7dee6fdf89f74
Martin <martin.barner@impact-initiatives.org>
2019-05-27 10:34:52 +0300

    apply ap

--- MCNA_dry_run.R
+++ MCNA_dry_run.R
@@ -17,15 +17,45 @@ questions$constraint <- tolower(questions$constraint)
 questions$calculation <- tolower(questions$calculation)
 questions$choice_filter <- tolower(questions$choice_filter)
 
+
+
 choices <- read.csv("input/kobo_choices.csv", stringsAsFactors=F, check.names=F)
+
 names(choices) <- tolower(names(choices))
 choices$name <- tolower(choices$name)
 choices$filter <- tolower(choices$filter)
 
-# debugonce(xlsformfill:::remove_skipped_values)
-response <- xlsform_fill(questions,choices,10)
-warnings()
-response[,1:10]
 
-response %>% new_recoding(source=how_much_debt, target=hh_with_debt_value)
->>>>>>> 0e3a00c6d7cfde6b3725a64cdf7bee600e6240bc
+response <- xlsform_fill(questions,choices,10)
+
+
+
+response %>%
+  new_recoding(source=how_much_debt,
+               target=hh_with_debt_value)
+
+
+
+
+
+names(response)<-to_alphanumeric_lowercase(names(response))
+questionnaire <- load_questionnaire(response,questions,choices)
+
+debugonce(map_to_datatypes)
+analysisplan_generic<-make_analysisplan_all_vars(response,
+                                                 questionnaire = questionnaire)
+
+
+results<-from_analysisplan_map_to_output(data = response,
+                                analysisplan = analysisplan_generic,weighting = function(x){rep(1,length(x))})
+
+
+
+
+charts <- results$results %>% lapply(map_to_labeled,questionnaire) %>% lapply(map_to_visualisation)
+
+
+charts[[80]]$ggplot
+
+
+
