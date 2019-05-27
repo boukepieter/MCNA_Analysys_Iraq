98716865bde6ee09348a93d32d24521f94d7c73f
Martin <martin.barner@impact-initiatives.org>
2019-05-27 15:19:34 +0300

    analysisplan simplified

--- MCNA_dry_run.R
+++ MCNA_dry_run.R
@@ -25,41 +25,60 @@ choices$name <- tolower(choices$name)
 choices$filter <- tolower(choices$filter)
 
 
-response <- xlsform_fill(questions,choices,100)
+response <- xlsform_fill(questions,choices,1000)
 
 # horizontal operations
 
-r <- response %>% new_recoding(source=how_much_debt, target=hh_with_debt_value) %>% 
-  recode_to(TRUE,where.num.larger.equal = 505000,otherwise.to=FALSE) %>% 
-  end_recoding()
 
-r <- r %>%  new_recoding(target=hh_unemployed) %>% 
-  recode_to(FALSE,where=!(is.na(response$work) | is.na(response$actively_seek_work))) %>% 
-  recode_to(TRUE,where=(work == "no") & (actively_seek_work == "yes")) %>% 
-  end_recoding()
+r <- response %>%
+  new_recoding(source=how_much_debt, target=hh_with_debt_value) %>% 
+  recode_to(0.25,where.num.larger.equal = 505000,otherwise.to=0) %>% 
 
-r <- r %>% new_recoding(source=reasons_for_debt, target=hh_unable_basic_needs) %>% 
-  recode_to(TRUE, where.selected.any = c("health","food","education","basic_hh_expenditure"), otherwise.to=FALSE) %>% 
-  end_recoding()
+  new_recoding(target=hh_unemployed) %>% 
+  recode_to(0 ,where=!(is.na(response$work) | is.na(response$actively_seek_work))) %>% 
+  recode_to(0.5,where=(work == "no") & (actively_seek_work == "yes")) %>% 
+
+  new_recoding(source=reasons_for_debt, target=hh_unable_basic_needs) %>% 
+  recode_to(0.25, where.selected.any = c("health","food","education","basic_hh_expenditure"), otherwise.to=0) %>% 
+  
+  end_recoding %>% 
+  mutate(score_livelihoods = hh_with_debt_value+hh_unemployed+hh_unable_basic_needs)
 
 
 
 # Prepare analysis
 
-names(response)<-to_alphanumeric_lowercase(names(response))
-questionnaire <- load_questionnaire(response,questions,choices)
+names(r)<-to_alphanumeric_lowercase(names(r))
+questionnaire <- load_questionnaire(r,questions,choices)
 
 
 # make analysisplan
 
-analysisplan<-make_analysisplan_all_vars(response,
-                                         questionnaire,
-                                         independent.variable = "type_hh")
+analysisplan<-make_analysisplan_all_vars(r,
+                                         questionnaire
+                                         # ,independent.variable = "type_hh",
+                                         # repeat.for.variable = "governorate_mcna"
+                                         )
+
+
+
 
 
 # vertical operations:
 
-results<-from_analysisplan_map_to_output(data = response,
+samplingframe <- load_samplingframe("./input/Strata_clusters_population.csv")
+samplingframe$stratum <- 
+
+# r$strata<-paste0(r$district_mcna,r$id)
+# 
+# weight_fun <- map_to_weighting(sampling.frame = samplingframe,
+#                  sampling.frame.population.column = "population",
+#                  sampling.frame.stratum.column = stratum,
+#                  data.stratum.column = "strata")
+
+
+
+results<-from_analysisplan_map_to_output(data = r,
                                          analysisplan = analysisplan,
                                          weighting = function(x){rep(1,nrow(x))},
                                          questionnaire = questionnaire)
