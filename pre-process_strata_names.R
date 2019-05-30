c10ba653f4fffaf368adf0abc1dc72b4e4a827ec
boukepieter <boukepieter@gmail.com>
2019-05-30 14:51:53 +0300

    modifying pre-process step

--- pre-process_strata_names.R
+++ pre-process_strata_names.R
@@ -10,7 +10,23 @@ strata_clusters <- strata_clusters %>% mutate(Stratum = paste0(District,popgroup
 write.csv(strata_clusters,"input_modified/Strata_clusters_population.csv", row.names=F)
 
 
-choices <- read.csv("input/kobo_choices.csv", stringsAsFactors = FALSE)
-choices %>% 
-  mutate(name = ifelse(list_name=="District_MCNA", to_alphanumeric_lowercase(label), name)) %>% 
-  write.csv("input_modified/kobo_choices2.csv", row.names=F, quote=F,na="")
+# questions <- read.csv("c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/survey.csv", stringsAsFactors=F, check.names=F)
+# questions$type <- tolower(questions$type)
+# questions$name <- tolower(questions$name)
+# questions$relevant <- tolower(questions$relevant)
+# questions$constraint <- tolower(questions$constraint)
+# questions$calculation <- tolower(questions$calculation)
+# questions$choice_filter <- tolower(questions$choice_filter)
+# write.csv(questions, "c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/survey_tolower.csv", row.names=F)
+# 
+# choices <- read.csv("c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/choices.csv", stringsAsFactors=F, check.names=F)
+# choices$list_name <- tolower(choices$list_name)
+# choices$name <- tolower(choices$name)
+# choices$Filter <- tolower(choices$Filter)
+# write.csv(choices, "c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/choices_tolower.csv", row.names=F)
+# 
+# districts <- read.csv("c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/districts.csv", stringsAsFactors=F, check.names=F)
+# districts$list_name <- tolower(districts$list_name)
+# districts$name <- to_alphanumeric_lowercase(districts$label)
+# districts$filter <- to_alphanumeric_lowercase(districts$filter)
+# write.csv(districts, "c:/Users/REACH-IRQ-GIS/Documents/201905 MCNA KoBo/districts_tolower.csv", row.names=F)
