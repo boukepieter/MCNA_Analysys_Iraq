9c4c60a170e5b50255ecd57b5f7e5e04f8cfc252
boukepieter <boukepieter@gmail.com>
2019-06-27 17:43:49 +0300

    added functions to log cleaning changes, execute the cleaning changes and one to check if interviews are inside cluster areas

--- data_cleaning/basic_tests.R
+++ data_cleaning/basic_tests.R
@@ -1,56 +1,73 @@
 #devtools::install_github("boukepieter/cleaninginspectoR", build_opts=c())
 library(cleaninginspectoR)
-
+library(plotKML)
+library(rgeos)
 library(xlsx)
-source("functions.R")
-dir <- "raw_data/20190620_test"
-kobo.xlsx.to.csv(dir, anonymise=F)
+library(raster)
+library(sf)
+Sys.setlocale("LC_ALL","Arabic")
 
-data <- read.csv(sprintf("%s/parent.csv",dir), stringsAsFactors = F)
+setwd("data_cleaning")
+source("cleaning_functions.R")
+WGS84 <- crs("+init=epsg:4326")
+dir <- "raw_data/20190627"
+kobo.xlsx.to.csv(dir, "(FINAL) Iraq MCNA Version VII", anonymise=F)
+
+data <- read.csv(sprintf("%s/parent.csv",dir), stringsAsFactors = F, encoding = "UTF-8")
 child <- read.csv(sprintf("%s/child.csv",dir), stringsAsFactors = F)
 
 #unique uuid's
 find_duplicates_uuid(data)
 
+inspect_all(data, uuid.column.name = "X_uuid")
+
 #interview speed
-as.POSIXct(data$end,format="%Y-%m-%dT%H:%M:%OS") - as.POSIXct(data$start,format="%Y-%m-%dT%H:%M:%OS")
+times <- as.POSIXct(data$end,format="%Y-%m-%dT%H:%M:%OS") - as.POSIXct(data$start,format="%Y-%m-%dT%H:%M:%OS")
+median(times)
 
 #shortest path (# of NA's)
-apply(data,1,FUN=function(x){length(which(is.na(x)))})
+NAs <- apply(data,1,FUN=function(x){length(which(is.na(x)))})
+overview_times <- data.frame(enumerator=data$enumerator_num, time=times, family_size=data$num_family_member,
+                             NAs=NAs)
+write.csv(overview_times, sprintf("%s/overview.csv",dir), row.names = F)
 
 #pop group
 data %>% mutate(total_pop_cat=sum(calc_idp,calc_returnee,calc_host,na.rm=T)) %>% 
-  select(total_pop_cat) ## ISSUE, TO SOLVE
+  dplyr::select(total_pop_cat) ## ISSUE, TO SOLVE
 data <- data %>% mutate(population_group = ifelse(calc_idp == 1, "idp", ifelse(calc_returnee == 1, "returnee", 
                                                                      ifelse(calc_host == 1, "host", NA)))) 
+#date check
+submission_date_to_check <- c("2019-06-27")
+filtered_data <- data %>% filter(date_assessment == submission_date_to_check) 
+sprintf("fraction of data on date %s: %f%%", submission_date_to_check, nrow(filtered_data) / nrow(data) * 100)
 
 #right locations
-library(sf)
-load("sample_points/sample_points_idp.RData")
-load("sample_points/sample_points_r.RData")
-load("sample_points/sample_points_host.RData")
-samplepoints <- list(host = sample_points_host, idp = sample_points_idp, r = sample_points_r)
+points.inside.cluster(data = data, samplepoints_file = "sample_points/samplepoints.RData",
+                      sampleareas_file = "sample_points/sample_areas.RData")
 
-submission_date_to_check <- c("2019-06-13")
-filtered_data <- data %>% filter(date_assessment == submission_date_to_check) 
-clusters_at_date <- filtered_data %>% select(cluster_location_id) %>% table %>% names
-pop_groups_at_date <- filtered_data %>% select(population_group) %>% table %>% names
+log <- log.cleaning.change(uuid = data_on_cluster$X_uuid, old.value = data_on_cluster$cluster_location_id,
+                          new.value = clusters_at_date[2], question.name = "cluster_location_id",
+                          issue = "wrong cluster selected, the right one is checked with the gps points", 
+                          dir = dir)
+execute.cleaning.changes(dir)
 
-cluster_no_to_check <- 1
-interview_locations <- filtered_data %>% 
-  filter(cluster_location_id == clusters_at_date[cluster_no_to_check]) %>% 
-  select(c(X_gpslocation_longitude,X_gpslocation_latitude))
-cluster <- strsplit(names(clusters_at_date)[cluster_no_to_check],"_")[[1]][4]
-cluster_points <- sample_points_idp %>% as("sf") %>% filter(startsWith(label, cluster))
+kml_open(file.name=sprintf("maps/%s",clusters_at_date[cluster_no_to_check]),
+         folder.name=clusters_at_date[cluster_no_to_check], kml_open=F)
+plotKML(cluster_area)
+plotKML(cluster_points)
+plotKML(interview_locations_geo, colour="cluster")
 
+
+
+#TODO(visualize cluster_area + cluster_points + interview_locations)
 
 #outliers check in integer values
-data$protection.birth_cert_missing_amount > data$household_roster.num_hh_member
-data$protection.id_card_missing_amount > data$household_roster.num_hh_member
-data$household_roster.num_family_member > data$household_roster.num_hh_member
-data$household_roster.num_hh_member > 25
-data$wash.refill_times > 10
-data$wash.people_share_tank > 20
+data$birth_cert_missing_amount_a1 + data$birth_cert_missing_amount_u1 > data$num_hh_member
+data$id_card_missing_amount > data$num_hh_member
+data$num_family_member > data$num_hh_member
+data$num_hh_member > 25
+data$refill_times > 10
+data$people_share_tank > 20
 
-data$household_roster.num_family_member
+data$num_family_member
 apply(data$X_uuid,c(1,2),FUN=function(x){child$X_uuid})
--- data_cleaning/cleaning_functions.R
+++ data_cleaning/cleaning_functions.R
@@ -1,7 +1,7 @@
-kobo.xlsx.to.csv <- function(dir, anonymise=F, anonymise_cols = NULL) {
+kobo.xlsx.to.csv <- function(dir, sheetName, anonymise=F, anonymise_cols = NULL) {
   files <- list.files(dir, pattern="*.xlsx")
   parent <- read.xlsx(paste(dir,files[1],sep="/"),
-                      sheetName = "Iraq MCNA VII_HH Survey")
+                      sheetName = sheetName)
   child <- read.xlsx(paste(dir,files[1],sep="/"),
                      sheetName = "member")
   
@@ -14,7 +14,80 @@ kobo.xlsx.to.csv <- function(dir, anonymise=F, anonymise_cols = NULL) {
     parent_ano <- parent[,-anonymise_cols]
     write.csv(parent_ano,paste0(dir,"/parent_anonymised.csv"))
   } else {
-    write.csv(parent,paste0(dir,"/parent.csv"))
+    write.csv(parent,paste0(dir,"/parent.csv"), row.names = F)
   }
-  write.csv(child,paste0(dir,"/child.csv"))
+  write.csv(child,paste0(dir,"/child.csv"), row.names = F)
+}
+
+log.cleaning.change <- function(uuid, old.value, question.name, new.value, issue=NULL,
+                                dir) {
+  if (file.exists(sprintf("%s/cleaning_logbook.csv",dir))){
+    log <- read.csv(log_file, stringsAsFactors = F)
+  } else {
+    log <- data.frame(uuid = character(), question.name = character(), issue = character(), feedback = character(), 
+                      changed = logical(), old.value = character(), new.value = character(), stringsAsFactors = F)
+  }
+  log[nrow(log)+1,"uuid"] <- uuid
+  log$question.name[nrow(log)] <- question.name
+  log$old.value[nrow(log)] <- old.value
+  log$new.value[nrow(log)] <- new.value
+  log$issue[nrow(log)] <- issue
+  write.csv(log, sprintf("%s/cleaning_logbook.csv",dir), row.names = F)
+  return(log)
+}
+
+execute.cleaning.changes <- function(dir, uuid_column=NULL) {
+  if (!file.exists(sprintf("%s/cleaning_logbook.csv",dir))){
+    stop("no cleaning file found")
+  }
+  log <- read.csv(sprintf("%s/cleaning_logbook.csv",dir), stringsAsFactors = F)
+  data <- read.csv(sprintf("%s/parent.csv",dir), stringsAsFactors = F, encoding = "UTF-8")
+  match <- grep(pattern = "uuid", x = names(data))
+  if (is.null(uuid_column)){
+    if (length(match) < 1) {
+      stop("cannot find uuid column")
+    } else if (length(match) > 1) {
+      stop("multiple uuid columns found")
+    } else {
+      uuid_column <- names(data)[match]
+    }
+  }
+  
+  for(i in 1:nrow(log)){
+    data[which(data[,uuid_column] == log$uuid[i]), log$question.name] <- log$new.value
+    log$changed <- TRUE
+  }
+  write.csv(log, sprintf("%s/cleaning_logbook.csv", dir), row.names = F)
+  write.csv(data, sprintf("%s/parent_cleaned.csv", dir), row.names = F)
+}
+
+points.inside.cluster <- function(data, samplepoints_file, sampleareas_file) {
+  load(samplepoints_file)
+  load(sampleareas_file)
+  samplepoints[["returnee"]] <- samplepoints[["r"]]
+  sample_areas[["returnee"]] <- sample_areas[["r"]]
+  
+  pop_groups <- data %>% dplyr::select(population_group) %>% table %>% names
+  for (i in 1:length(pop_groups)){
+    data_pop <- data %>% filter(population_group == pop_groups[i])
+    
+    clusters <- data_pop %>% dplyr::select(cluster_location_id) %>% table %>% names
+    for (j in 1:length(clusters)){
+      data_on_cluster <- data_pop %>% 
+        filter(cluster_location_id == clusters[j])
+      interview_locations <- data_on_cluster %>% 
+        dplyr::select(c(X_gpslocation_longitude,X_gpslocation_latitude))
+      interview_locations_geo <- SpatialPointsDataFrame(interview_locations,interview_locations, proj4string = WGS84)
+      interview_locations_geo$cluster <- clusters[j]
+      
+      cluster <- strsplit(clusters[j],"_")[[1]][4]
+      cluster_points <- samplepoints[[pop_groups[i]]] %>% 
+        as("sf") %>% filter(startsWith(label, cluster)) %>% as("Spatial")
+      cluster_area <- sample_areas[[pop_groups[i]]] %>% 
+        as("sf") %>% filter(startsWith(label, cluster)) %>% as("Spatial")
+      
+      cat(sprintf("Interviews for %s - %s are inside the cluster area: %s\n", clusters[j], pop_groups[i],
+              gContains(cluster_area,interview_locations_geo)))
+    }
+  }
 }
